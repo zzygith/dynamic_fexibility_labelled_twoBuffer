157b060d0deb97e07a873c29032f838407f98467
zzygith <58028642+zzygith@users.noreply.github.com>
2023-01-24 16:29:16 -0700

    Update main.py

--- src/networks/main.py
+++ src/networks/main.py
@@ -6,8 +6,6 @@ from .cifar10_LeNet_elu import CIFAR10_LeNet_ELU, CIFAR10_LeNet_ELU_Autoencoder
 def build_network(net_name):
     """Builds the neural network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU')
-    assert net_name in implemented_networks
 
     net = None
 
