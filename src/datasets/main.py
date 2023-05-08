ede577bf805c41f2812023ec3ac02f7d21451087
Zhongyu Zhang <zhongyu4@engineering.ualberta.ca>
2023-05-08 13:54:03 -0600

    add read data code for multipule den data

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -1,6 +1,7 @@
 from .mnist import MNIST_Dataset
 from .cifar10 import CIFAR10_Dataset
 from .mine import MINE_Dataset
+from .mineMultipleDen import MINEMD_Dataset
 
 
 def load_dataset(dataset_name, data_path, normal_class):
@@ -17,5 +18,8 @@ def load_dataset(dataset_name, data_path, normal_class):
 
     if dataset_name == 'mine' or dataset_name == 'mine_heater_1d':
         dataset = MINE_Dataset(root=data_path, normal_class=normal_class)
+
+    if dataset_name == 'mine_reactorCooler_2d':
+        dataset = MINEMD_Dataset(root=data_path, normal_class=normal_class)
 
     return dataset
